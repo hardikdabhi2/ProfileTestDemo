b7df68bf21ada3db5b952811233ec9cb584a4e96
Hardik <hardik_isl@Mac.local>
2019-01-22 15:05:58 +0530

    User Profile UI/UX

---
+++ test/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Hardik on 08/01/19.
+//  Copyright © 2019 HardikDabhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageProfile:UIImageView!
+    @IBOutlet weak var labelName:UILabel!
+    @IBOutlet weak var labelEmail:UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK: - Button Action
+    @IBAction func BtnActionEditProfile(_ sender:UIButton)
+    {
+        
+    }
+    @IBAction func BtnActionChangePassword(_ sender:UIButton)
+    {
+        
+    }
+    @IBAction func BtnActionLogut(_ sender:UIButton)
+    {
+        
+    }
+
+}
+
